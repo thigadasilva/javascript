983cb0e7e6f82e8c114a302139af0755093bc83d
Thiago da Silva <thiggasto@gmail.com>
2025-11-18 16:27:11 -0300

    Delete NODE directory

--- NODE/routes/viewRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const viewController = require('../controllers/viewController')
-
-router.get('/', viewController.listar);
-
-module.exports = router;
